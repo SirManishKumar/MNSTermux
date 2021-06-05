000cdd6b761b89be94aa6a75a6e38fdc45a4de53
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-06-05 12:42:08 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,148 @@
 #!/bin/bash
 apt install python -y
 pip install lolcat
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- sIRMANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SiRMANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIrMANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRmANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMaNISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMAnISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANiSHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANIsHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANIShKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANISHkUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANISHKuMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANISHKUmAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANISHKUMaR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANISHKUMAr"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SIRMANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- sIRMANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SiRMANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirMANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirmANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirMaNISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManiSHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManisHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishkUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKuMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKumAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKumaR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKumar"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKumar."
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKumar.."
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKumar..."
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKumar...."
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKumar....."
+figlet -f slant "Connected" | lolcat
+sleep 1
+clear
 pkg upgrade -y
 clear
 echo " "
